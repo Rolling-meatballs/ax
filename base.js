705b6f797fb8b2cde91adcd25833a93194a815db
Murphy <yangfan@izhihuo.com>
2020-09-22 21:03:13 +0800

    62

--- base.js
+++ base.js
@@ -24,13 +24,41 @@ var Paddle = function() {
 }
 
 var PeachGame = function() {
-    var g = {}
+    var g = {
+        actions: {},
+        keydowns: {},
+    }
     var canvas = document.querySelector('#id-canvas')
     var context = canvas.getContext('2d')
     g.canvas = canvas
     g.context = context
+    //draw
+    g.drawImage = function(peachImage) {
+        g.context.drawImage(peachImage.image, peachImage.x, peachImage.y)
+    }
+
+    window.addEventListener('keydown', function(event){
+        g.keydowns[event.key] = true
+    })
+
+    window.addEventListener('keyup', function(event){
+        g.keydowns[event.key] = false
+    })
+
+    g.registerAction = function(key, callback) {
+        g.actions[key] = callback
+    }
 
     setInterval(function() {
+        // event
+        var actions = Object.keys(g.actions)
+        for (var i = 0; i < actions.length; i++) {
+            var key = actions[i]
+            if(g.keydowns[key]) {
+                // if key was passdown, run register action
+                g.actions[key]()
+            }
+        }
         //update
         g.update()
         // clear
@@ -50,41 +78,22 @@ var __main = function() {
     var Rightdown = false
     var Leftdown = false
 
-    // events
-    window.addEventListener('keydown', function(event){
-        var k = event.key
-        log('keydown')
-        if (k == 'a') {
-            Rightdown = true
-        } else if (k == 'd') {
-            Leftdown = true
-        }
+    game.registerAction('a', function() {
+        paddle.moveLeft()
     })
 
-    // events
-    window.addEventListener('keyup', function(event){
-        var k = event.key
-        log('keyup')
-        if (k == 'a') {
-            Rightdown = false
-        } else if (k == 'd') {
-            Leftdown = false
-        }
+    game.registerAction('d', function() {
+        paddle.moveRight()
     })
 
     game.update = function() {
         // update x
         // log('update', Leftdown, Rightdown)
-        if (Leftdown) {
-            paddle.moveLeft()
-        } else if (Rightdown) {
-            paddle.moveRight()
-        }
     }
 
     game.draw = function() {
         // draw
-        game.context.drawImage(paddle.image, paddle.x, paddle.y)
+        game.drawImage(paddle)
     }
 
 }
