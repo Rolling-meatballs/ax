e3b67a8d318e74548e0d1fc425c0f2a69adfc8fc
Murphy <yangfan__8123@163.com>
2020-10-05 00:13:12 +0800

    base ball

--- base.js
+++ base.js
@@ -11,14 +11,22 @@ var Paddle = function() {
     var o = {
         image: image,
         x: 100,
-        y: 200,
-        speed: 5,
+        y: 250,
+        speed: 15,
     }
     o.moveLeft = function() {
         o.x -= o.speed
     }
     o.moveRight = function() {
         o.x += o.speed
+    }
+    o.collide = function(ball) {
+      if (ball.y + ball.image.height > o.y) {
+        if (ball.x > o.x && ball.x < o.x + o.image.width) {
+          return true
+        }
+      }
+      return false
     }
     return o
 }
@@ -35,10 +43,8 @@ var Ball = function() {
     }
     o.fire = function() {
         o.fired = true
-        // log('fire')
     }
     o.move = function() {
-        log('here is ball')
         if (o.fired) {
             // log('move')
             if (o.x < 0 || o.x > 400) {
@@ -55,35 +61,88 @@ var Ball = function() {
     return o
 }
 
-var leftDown = fales
-var rightDown = fales
-
-
-// events
-window.addEventListener('keydown', function(event){
-    var k = event.key
-    // log(event)
-    if (k == 'a') {
-        leftDown = true
-    } else if (k == 'd') {
-         rightDown = true
-    }
-})
-
-window.addEventListener('keyup', function(event){
-  var k = event.key
-
-
-})
-
-setInterval(function(){
-  // update x and y
-  if(leftDown) {
-    x -= 10
-  } else {
-    x += 10
+var AGame = function() {
+  var g = {
+    actions: {},
+    keydowns: {},
+    // update: function(){
+    // },
   }
+  var canvas = document.querySelector('#id-canvas')
+  var context = canvas.getContext('2d')
+  g.canvas = canvas
+  g.context = context
   // draw
-  context.clearRect(0, 0, canvas.clientWidth, canvas.height)
-  context.drawImage(img, x, y)
-}, 1000/30)
+  g.drawImage = function(aImage) {
+    g.context.drawImage(aImage.image, aImage.x, aImage.y)
+  }
+
+  // events
+  window.addEventListener('keydown', function(event) {
+    g.keydowns[event.key] = true
+  })
+  window.addEventListener('keyup', function(event) {
+    g.keydowns[event.key] = false
+  })
+  //
+  g.registerAction = function(key, callback) {
+    log('registerAction', key)
+    g.actions[key] = callback
+  }
+  // timer
+
+  setInterval(function() {
+    // events
+    var actions = Object.keys(g.actions)
+    for (var i = 0; i < actions.length; i++) {
+       var key = actions[i]
+       if(g.keydowns[key]) {
+         // 如果按键按下，调用注册的action
+         g.actions[key]()
+       }
+    }
+    // update
+    g.update()
+    //clear
+    g.context.clearRect(0, 0, g.canvas.width, g.canvas.height)
+    //draw
+    g.draw()
+  }, 1000/30)
+
+  return g;
+}
+
+var __main = function() {
+  var game = AGame()
+
+  var paddle = Paddle()
+  var ball = Ball()
+
+  game.registerAction('a', function() {
+    paddle.moveLeft()
+  })
+  game.registerAction('d', function() {
+    paddle.moveRight()
+  })
+  game.registerAction('f', function() {
+    ball.fire()
+  })
+
+  game.update = function() {
+    ball.move()
+    // 判断相撞
+    if (paddle.collide(ball)) {
+      // 这里调用一个函数
+      ball.speedY *= -1
+    }
+  }
+
+  game.draw = function() {
+    // draw
+    game.drawImage(paddle)
+    game.drawImage(ball)
+  }
+
+}
+
+__main()
