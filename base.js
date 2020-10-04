dc65f9da2149a8eeddd46ddf9779c34996ed9c84
Murphy <yangfan__8123@163.com>
2020-10-04 15:33:57 +0800

    some

--- base.js
+++ base.js
@@ -13,17 +13,35 @@ img.onload = function(){
     context.drawImage(img, x, y)
 }
 
+var leftDown = fales
+var rightDown = fales
+
+
 // events
 window.addEventListener('keydown', function(event){
     var k = event.key
     // log(event)
     if (k == 'a') {
-        x -= 5
-        context.clearRect(0, 0, canvas.clientWidth, canvas.height)
-        context.drawImage(img, x, y)
+        leftDown = true
     } else if (k == 'd') {
-        x += 5
-        context.clearRect(0, 0, canvas.clientWidth, canvas.height)
-        context.drawImage(img, x, y)
+         rightDown = true
     }
 })
+
+window.addEventListener('keyup', function(event){
+  var k = event.key
+
+
+})
+
+setInterval(function(){
+  // update x and y
+  if(leftDown) {
+    x -= 10
+  } else {
+    x += 10
+  }
+  // draw
+  context.clearRect(0, 0, canvas.clientWidth, canvas.height)
+  context.drawImage(img, x, y)
+}, 1000/30)
